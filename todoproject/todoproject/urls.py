7219e053d1296d49e3293821fbdb45a6e989eed3
Alok Singh <112793361+aloksingh72@users.noreply.github.com>
2025-06-09 18:10:23 +0530

    Update urls.py

--- todoproject/todoproject/urls.py
+++ todoproject/todoproject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+"""
+urls patterns for admin
+"""
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('todoapp.urls')),
